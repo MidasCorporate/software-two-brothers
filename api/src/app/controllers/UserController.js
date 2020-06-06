8b6e8b5994064f1c844cb96e71ed5c2bea713660
alessandro-silva <alessandro.rds03@gmail.com>
2020-06-06 11:27:10 -0400

    add api

---
+++ api/src/app/controllers/UserController.js
@@ -0,0 +1,13 @@
+import User from '../schemas/User';
+
+class UserController {
+  async index(req, res) {
+    return res.json();
+  }
+
+  async store(req, res) {
+    return res.json();
+  }
+}
+
+export default new UserController();
