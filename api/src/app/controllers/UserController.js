bfb33bd4ca90a086ed53f95e9e9940eb8d7134c1
alessandro-silva <alessandro.rds03@gmail.com>
2020-06-07 15:54:30 -0400

    add files and add about

--- api/src/app/controllers/UserController.js
+++ api/src/app/controllers/UserController.js
@@ -3,15 +3,17 @@ import User from '../schemas/User';
 
 class UserController {
   async index(req, res) {
-    return res.json();
+    const users = await User.find();
+
+    return res.json(users);
   }
 
   async store(req, res) {
-    const { name, email, password } = req.body;
+    const { name, email, password, about } = req.body;
 
-    const UserExists = await User.findOne({ email });
+    const userExists = await User.findOne({ email });
 
-    if (UserExists) {
+    if (!userExists) {
       return res.status(400).json({ error: 'User already exists' });
     }
 
@@ -19,9 +21,26 @@ class UserController {
       name,
       email,
       password: bcrypt.hashSync(password, 8),
+      about,
     });
 
     return res.json(userCreated);
+  }
+
+  async update(req, res) {
+    const { about } = req.body;
+
+    const userExists = await User.findOne({ _id: req.userId });
+
+    if (!userExists) {
+      return res.status(400).json({ error: 'User already exists' });
+    }
+
+    await userExists.updateOne({
+      about,
+    });
+
+    return res.json(userExists);
   }
 }
 
--- api/src/app/schemas/User.js
+++ api/src/app/schemas/User.js
@@ -14,6 +14,10 @@ const UserSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    about: {
+      type: String,
+      required: true,
+    },
   },
   {
     timestamps: true,
