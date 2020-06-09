bec3d9bb03a34330f2c9a16dc0c32ff5d175c3c3
alessandro-silva <alessandro.rds03@gmail.com>
2020-06-09 18:00:11 -0400

    update

--- web/src/pages/client/Dashboard/Contact/index.js
+++ web/src/pages/client/Dashboard/Contact/index.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import { Form } from '@unform/web';
+import { toast } from 'react-toastify';
 import { FaWhatsapp } from 'react-icons/fa';
 
+import api from '~/services/api';
+
+import Input from '~/components/Input';
+import Textarea from '~/components/Textarea';
 import { Wrapper, Container } from './styles';
 
 function Contact() {
+  async function handleSubmit({ name, email, cel, tel, message }) {
+    await api.post('contacts', {
+      name,
+      email,
+      cel,
+      tel,
+      message,
+    });
+
+    toast.success('Messagem enviado com sucesso');
+  }
+
   return (
     <Wrapper>
       <Container>
@@ -15,22 +33,22 @@ function Contact() {
             (67) 9 9840-8117
           </p>
         </div>
-        <form>
+        <Form onSubmit={handleSubmit}>
           <div>
-            <input placeholder="nome" />
+            <Input name="name" placeholder="nome" />
           </div>
           <div>
-            <input type="email" placeholder="E-mail" />
+            <Input name="email" type="email" placeholder="E-mail" />
           </div>
           <div className="contact">
-            <input placeholder="celular" />
-            <input placeholder="telefone" />
+            <Input name="cel" placeholder="celular" />
+            <Input name="tel" placeholder="telefone" />
           </div>
           <div>
-            <textarea rows="5" placeholder="Messagem" />
+            <Textarea name="message" placeholder="Messagem" />
           </div>
           <button type="submit">Enviar</button>
-        </form>
+        </Form>
       </Container>
     </Wrapper>
   );
